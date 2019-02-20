18feda4b2c806d9d38f7b06cc56f7fcde7b346e2
Yukiji Nakatsuka <lunarbase@outlook.com>
2019-02-20 01:03:09 +0000

    Automatically backed up by Learn

---
+++ app/models/show.rb
@@ -0,0 +1,17 @@
+class Show < ActiveRecord::Base  
+  def self.highest_rating 
+    self.maximum(:rating)
+  end 
+  
+  def self.most_popular_show
+    self.where(self.highest_rating).first
+  end 
+  
+  def self.lowest_rating
+    self.minimum(:rating)
+  end
+  
+  def self.least_popular_show 
+    self.where(self.lowest_rating).order(:rating, :desc).last
+  end 
+end 
